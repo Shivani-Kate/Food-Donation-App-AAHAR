1f7af7f8cb2b74b6fd275e3dd1a44b6658549f16
Shivani Kate <shivanikate381@gmail.com>
2026-06-15 15:44:48 +0530

    Fix MongoDB Atlas

--- AAHAR1-main/backend/config/db.js
+++ AAHAR1-main/backend/config/db.js
@@ -1,12 +1,25 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.mongodb+srv://shivanikate381_db_user:<db_password>@cluster0.ivgbxfd.mongodb.net/?appName=Cluster0); {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(" MongoDB Connected");
+//   } catch (err) {
+//     console.error("MongoDB Error:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/aahaar", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(" MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Error:", err.message);
     process.exit(1);
